19b3c8744898f74dc5f8133bb2c2892f851642e2
DCR430 <daniel.c.reyes30@gmail.com>
2021-02-10 21:43:59 -0500

    render square

--- src/index.js
+++ src/index.js
@@ -36,34 +36,34 @@ function Game() {
     }
   }
 
-//   function renderSquare(i) {
-//     return (
-//       <Square
-//         value={squares[i]}
-//         onClick={() => {
-//           if (squares[i] != null || winner != null) {
-//             return;
-//           }
-//           const nextSquares = squares.slice();
-//           nextSquares[i] = nextSymbol;
-//           setSquares(nextSquares);
+  function renderSquare(i) {
+    return (
+      <Square
+        value={squares[i]}
+        onClick={() => {
+          if (squares[i] != null || winner != null) {
+            return;
+          }
+          const nextSquares = squares.slice();
+          nextSquares[i] = nextSymbol;
+          setSquares(nextSquares);
 
-//           setIsXNext(!isXNext); // toggle turns
-//         }}
-//       />
-//     );
-//   }
+          setIsXNext(!isXNext); // toggle turns
+        }}
+      />
+    );
+  }
 
-//   function renderRestartButton() {
-//     return (
-//       <Restart
-//         onClick={() => {
-//           setSquares(Array(9).fill(null));
-//           setIsXNext(true);
-//         }}
-//       />
-//     );
-//   }
+  function renderRestartButton() {
+    return (
+      <Restart
+        onClick={() => {
+          setSquares(Array(9).fill(null));
+          setIsXNext(true);
+        }}
+      />
+    );
+  }
 
 //   return (
 //     <div className="container">
