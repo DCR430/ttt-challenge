7f282340bd49e454599338288f104e3bdabad4e6
DCR430 <daniel.c.reyes30@gmail.com>
2021-02-11 20:36:50 -0500

    render finished

--- src/index.js
+++ src/index.js
@@ -36,23 +36,23 @@ function Game() {
     }
   }
 
-  // function renderSquare(i) {
-  //   return (
-  //     <Square
-  //       value={squares[i]}
-  //       onClick={() => {
-  //         if (squares[i] != null || winner != null) {
-  //           return;
-  //         }
-  //         const nextSquares = squares.slice();
-  //         nextSquares[i] = nextSymbol;
-  //         setSquares(nextSquares);
+  function renderSquare(i) {
+    return (
+      <Square
+        value={squares[i]}
+        onClick={() => {
+          if (squares[i] != null || winner != null) {
+            return;
+          }
+          const nextSquares = squares.slice();
+          nextSquares[i] = nextSymbol;
+          setSquares(nextSquares);
 
-  //         setIsXNext(!isXNext); // toggle turns
-  //       }}
-  //     />
-  //   );
-  // }
+          setIsXNext(!isXNext); // toggle turns
+        }}
+      />
+    );
+  }
 
   // function renderRestartButton() {
   //   return (
