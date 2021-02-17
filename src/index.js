c4adca70990f3b2b386f2540a8056e85e80129d7
DCR430 <daniel.c.reyes30@gmail.com>
2021-02-17 16:24:52 -0500

    color

--- src/index.js
+++ src/index.js
@@ -94,33 +94,33 @@ function Game() {
 
 ReactDOM.render(<Game />, document.getElementById("root"));
 
-// function calculateWinner(squares) {
-//   console.log(squares)
-//   const possibleLines = [
-//     [0, 1, 2],
-//     [3, 4, 5],
-//     [6, 7, 8],
-//     [0, 3, 6],
-//     [1, 4, 7],
-//     [2, 5, 8],
-//     [0, 4, 8],
-//     [2, 4, 6]
-//   ];
+function calculateWinner(squares) {
+  console.log(squares)
+  const possibleLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
 
-//   for (let i = 0; i < possibleLines.length; i++) {
-//     const [a, b, c] = possibleLines[i];
-//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-//       return squares[a];
-//     }
-//   }
-//   return null;
-// }
+  for (let i = 0; i < possibleLines.length; i++) {
+    const [a, b, c] = possibleLines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
-// function isBoardFull(squares) {
-//   for (let i = 0; i < squares.length; i++) {
-//     if (squares[i] == null) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
+function isBoardFull(squares) {
+  for (let i = 0; i < squares.length; i++) {
+    if (squares[i] == null) {
+      return false;
+    }
+  }
+  return true;
+}
