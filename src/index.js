378e2ec55904f63401edae1b090e764eace45440
DCR430 <daniel.c.reyes30@gmail.com>
2021-05-16 19:07:53 -0400

    green

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ function Restart({ onClick }) {
 function Game() {
   const [ squares, setSquares ] = useState(Array(9).fill(null));
   const [ isXNext, setIsXNext ] = useState(true);
-  const nextSymbol = isXNext ? "blue" : "red";
+  const nextSymbol = isXNext ? "blue" : "green";
   const winner = calculateWinner(squares);
 
   function getStatus() {
