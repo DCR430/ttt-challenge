12e43294244bb9174637d0f0d701ded727ba7b86
DCR430 <daniel.c.reyes30@gmail.com>
2021-05-18 09:39:47 -0400

    DRAW 3

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ function Game() {
     if (winner) {
       return "You Won!! : " + winner;
     } else if (isBoardFull(squares)) {
-      return " ITS A Draw !";
+      // return " ITS A Draw !";
     } else {
       return "Next player: " + nextSymbol;
     }
